c4fc3bd640af749c90e2cae87337613ec597452b
CzPhantom10 <ps826105@gmail.com>
2026-03-22 00:15:16 +0530

    tailwind

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        accent: '#12c2b9'
+      }
+    }
+  },
+  plugins: []
+};
